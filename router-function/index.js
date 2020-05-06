7d10ea0b94ffe30319f24b7c8ef724fd93a5d435
Dmitry White <dmitry.b.white@gmail.com>
2020-05-06 23:37:07 +0300

    Refactor router functions

--- router-function/index.js
+++ router-function/index.js
@@ -1,48 +1,68 @@
-function selectTab(id) {
-  // remove selected class from all buttons
-  document
-    .querySelectorAll(".route")
-    .forEach((item) => item.classList.remove("selected"));
-  // select clicked element (visually)
-  document
-    .querySelectorAll("#" + id)
-    .forEach((item) => item.classList.add("selected"));
+function navigate(to) {
+  // Finally push state change to the address bar
+  window.history.pushState({ to }, `${to}`, `/page/${to}`);
 }
-function loadContent(id) {
-  // Update text "Content loading for {id}..."
+
+function changeTitle(to) {
+  // Update Title in Window's Tab
+  document.title = to;
+}
+
+function selectTab(to) {
+  // remove selected class from all buttons
+  const routeSections = document.querySelectorAll(".route");
+  routeSections.forEach((item) => item.classList.remove("selected"));
+
+  // select clicked element (visually)
+  const selectedTabs = document.querySelectorAll("#" + to);
+  selectedTabs.forEach((item) => item.classList.add("selected"));
+}
+
+function loadContent(to) {
+  // Update text "Content loading for {to}..."
   // Of course, here you would do you content loading magic
   // Perhaps run Fetch API to update resources
-  document.querySelector("#content").innerHTML =
-    "Content loading for /" + id + "...";
+  const contentSection = document.querySelector("#content");
+  contentSection.innerHTML = "Content loading for /" + to + "...";
 }
+
 function push(event) {
   // Get id attribute of the box or button or link clicked
-  let id = event.target.id;
-  // Visually select the clicked button/tab/box
-  selectTab(id);
-  // Update Title in Window's Tab
-  document.title = id;
-  // Load content for this tab/page
-  loadContent(id);
-  // Finally push state change to the address bar
-  window.history.pushState({ id }, `${id}`, `/page/${id}`);
+  const to = event.target.id;
+  console.log("to = ", to);
+
+  changeTitle(to);
+
+  selectTab(to);
+  loadContent(to);
+
+  navigate(to);
 }
-window.onload = (event) => {
-  // Add history push() event when boxes are clicked
-  window["home"].addEventListener("click", (event) => push(event));
-  window["about"].addEventListener("click", (event) => push(event));
-  window["gallery"].addEventListener("click", (event) => push(event));
-  window["contact"].addEventListener("click", (event) => push(event));
-  window["help"].addEventListener("click", (event) => push(event));
+
+const init = () => {
+  const homeSection = document.querySelector("#home");
+  const aboutSection = document.querySelector("#about");
+  const gallerySection = document.querySelector("#gallery");
+  const contactSection = document.querySelector("#contact");
+  const helpSection = document.querySelector("#help");
+
+  homeSection.addEventListener("click", (event) => push(event));
+  aboutSection.addEventListener("click", (event) => push(event));
+  gallerySection.addEventListener("click", (event) => push(event));
+  contactSection.addEventListener("click", (event) => push(event));
+  helpSection.addEventListener("click", (event) => push(event));
 };
-// Listen for PopStateEvent (Back or Forward buttons are clicked)
-window.addEventListener("popstate", (event) => {
+
+const listen = (event) => {
   // Grab the history state id
-  let stateId = event.state.id;
-  // Show clicked id in console (just for fun)
+  const stateId = event.state.to;
   console.log("stateId = ", stateId);
-  // Visually select the clicked button/tab/box
+
   selectTab(stateId);
-  // Load content for this tab/page
   loadContent(stateId);
-});
+};
+
+// Listen for PopStateEvent (Back or Forward buttons are clicked)
+window.addEventListener("popstate", listen);
+// Add history push() event when boxes are clicked
+window.onload = init;
