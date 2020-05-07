ed6062c25280636dc9bde621e9bed77308295d63
Dmitry White <dmitry.b.white@gmail.com>
2020-05-07 17:03:56 +0300

    Add find component by path funcitonality

--- router-spa/router.js
+++ router-spa/router.js
@@ -1,14 +1,32 @@
+const ROUTE_PATHS = {
+  HOME: "/",
+  ABOUT: "/about",
+  BLOG: "/blog",
+};
 const routes = [
-  { path: "/", component: HomeComponent },
-  { path: "/about", component: AboutComponent },
-  { path: "/blog", component: BlogComponent },
+  { path: ROUTE_PATHS.HOME, component: HomeComponent },
+  { path: ROUTE_PATHS.ABOUT, component: AboutComponent },
+  { path: ROUTE_PATHS.BLOG, component: BlogComponent },
 ];
 
-const parseLocation = () => location.hash.slice(1).toLowerCase() || "/";
+const parseLocation = () =>
+  location.hash.slice(1).toLowerCase() || ROUTE_PATHS.HOME;
+
+const findComponentByPath = (path, routes) => {
+  const pathRegex = new RegExp(`^\\${path}$`, "gm");
+
+  const findHandler = (route) => route.path.match(pathRegex);
+
+  const foundRoute = routes.find(findHandler) || undefined;
+
+  return foundRoute || {};
+};
 
 const router = () => {
   const path = parseLocation();
-  // TODO: Find the component based on the current path
+  const { component: ErrorComponent } = findComponentByPath(path);
   // TODO: If there's no matching route, get the "Error" component
   // TODO: Render the component in the "app" placeholder
 };
+
+findComponentByPath("/about", routes);
