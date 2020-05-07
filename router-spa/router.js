5c7da9bfd18ace76d64dcc58f023effc86215b2c
Dmitry White <dmitry.b.white@gmail.com>
2020-05-07 16:42:21 +0300

    Add parse location functionality

--- router-spa/router.js
+++ router-spa/router.js
@@ -3,3 +3,12 @@ const routes = [
   { path: "/about", component: AboutComponent },
   { path: "/blog", component: BlogComponent },
 ];
+
+const parseLocation = () => location.hash.slice(1).toLowerCase() || "/";
+
+const router = () => {
+  const path = parseLocation();
+  // TODO: Find the component based on the current path
+  // TODO: If there's no matching route, get the "Error" component
+  // TODO: Render the component in the "app" placeholder
+};
